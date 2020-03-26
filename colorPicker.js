0c61837ac32863486904ad1225570e1b6d85ee22
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-03-26 13:10:27 -0700

    Hue sliders change each sections own corresponding color swatch

--- colorPicker.js
+++ colorPicker.js
@@ -1,8 +1,11 @@
 // DOM
-var sliderHue = document.querySelectorAll(".slider-hue");
-var huePrimaryVal = document.querySelector(".hue-pri-value");
-var hueSecondaryVal = document.querySelector(".hue-2nd-value");
-var hueTertiaryVal = document.querySelector(".hue-tert-value");
+const sliderHue = document.querySelectorAll(".slider-hue");
+const huePrimaryVal = document.querySelector(".hue-pri-value");
+const hueSecondaryVal = document.querySelector(".hue-2nd-value");
+const hueTertiaryVal = document.querySelector(".hue-tert-value");
+const primaryColorSwatch = document.querySelector('.primary-color-swatch');
+const secondaryColorSwatch = document.querySelector('.secondary-color-swatch');
+const tertiaryColorSwatch = document.querySelector('.tertiary-color-swatch');
 
 // Default Values
 
@@ -13,24 +16,34 @@ var hueTertiaryVal = document.querySelector(".hue-tert-value");
 //     sliderHueVal.innerHTML = `H: ${this.value}`;
 // }
 
+const hueSliderValues = [];
 
 // Hue Sliders
 sliderHue.forEach(function (slider) {
+
+    
+
+    hueSliderValues.push(slider);
     huePrimaryVal.innerHTML = `H: ${slider.value}`;
     hueSecondaryVal.innerHTML = `H: ${slider.value}`;
     hueTertiaryVal.innerHTML = `H: ${slider.value}`;
 
     if (slider.name === 'hue-primary') {
         slider.oninput = () => {
-            huePrimaryVal.innerHTML = `H: ${slider.value}`;
+            huePrimaryVal.innerHTML = `H: ${hueSliderValues[0].value}`;
+            primaryColorSwatch.style.backgroundColor = `hsl(${hueSliderValues[0].value}, 100%, 50%)`;
         };
     }else if (slider.name === 'hue-secondary') {
         slider.oninput = () => {
-            hueSecondaryVal.innerHTML = `H: ${slider.value}`;
+            hueSecondaryVal.innerHTML = `H: ${hueSliderValues[1].value}`;
+            secondaryColorSwatch.style.backgroundColor = `hsl(${hueSliderValues[1].value}, 100%, 50%)`;
         };
     }else if (slider.name === 'hue-tertiary') {
         slider.oninput = () => {
-            hueTertiaryVal.innerHTML = `H: ${slider.value}`;
+            hueTertiaryVal.innerHTML = `H: ${hueSliderValues[2].value}`;
+            tertiaryColorSwatch.style.backgroundColor = `hsl(${hueSliderValues[2].value}, 100%, 50%)`;
         };
     }
 });
+
+console.log(hueSliderValues);
