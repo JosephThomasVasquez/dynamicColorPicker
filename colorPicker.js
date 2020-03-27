4b0691a593fba03c1c0acb9bd1592e04a2e65333
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-03-26 18:39:18 -0700

    Decided to create the html elements in javascript

--- colorPicker.js
+++ colorPicker.js
@@ -1,49 +1,80 @@
-// DOM
-const sliderHue = document.querySelectorAll(".slider-hue");
-const huePrimaryVal = document.querySelector(".hue-pri-value");
-const hueSecondaryVal = document.querySelector(".hue-2nd-value");
-const hueTertiaryVal = document.querySelector(".hue-tert-value");
-const primaryColorSwatch = document.querySelector('.primary-color-swatch');
-const secondaryColorSwatch = document.querySelector('.secondary-color-swatch');
-const tertiaryColorSwatch = document.querySelector('.tertiary-color-swatch');
+// // DOM
 
-// Default Values
+// Main div - container for all color pickers
+const sliderContainer = document.querySelector('.slidecontainer');
+
+// Create elements
+const colorTitleElement = document.createElement('h3');
+const primaryLabel = colorTitleElement.textContent = 'Primary Color';
+
+const hueSlider = document.createElement('input');
+const satSlider = document.createElement('input');
+const lightSlider = document.createElement('input');
+
+const colorSwatch = document.createElement('div');
+colorSwatch.classList.add('primary-color-swatch');
 
 
 
-// Update the current slider value (each time you drag the slider handle)
-// sliderHue.oninput = function() {
-//     sliderHueVal.innerHTML = `H: ${this.value}`;
-// }
+const hueAttributes = {
+    id: 'hue1',
+    type: 'range',
+    min: '1',
+    max: '360',
+    value: '180',
+    class: 'slider'
+}
 
-const hueSliderValues = [];
+const satAttributes = {
+    id: 'sat1',
+    type: 'range',
+    min: '1',
+    max: '100',
+    value: '50',
+    class: 'slider'
+}
 
-// Hue Sliders
-sliderHue.forEach(function (slider) {
+const lightAttributes = {
+    id: 'light1',
+    type: 'range',
+    min: '1',
+    max: '100',
+    value: '50',
+    class: 'slider'
+}
 
-    
 
-    hueSliderValues.push(slider);
-    huePrimaryVal.innerHTML = `H: ${slider.value}`;
-    hueSecondaryVal.innerHTML = `H: ${slider.value}`;
-    hueTertiaryVal.innerHTML = `H: ${slider.value}`;
+createColorPicker();
 
-    if (slider.name === 'hue-primary') {
-        slider.oninput = () => {
-            huePrimaryVal.innerHTML = `H: ${hueSliderValues[0].value}`;
-            primaryColorSwatch.style.backgroundColor = `hsl(${hueSliderValues[0].value}, 100%, 50%)`;
-        };
-    }else if (slider.name === 'hue-secondary') {
-        slider.oninput = () => {
-            hueSecondaryVal.innerHTML = `H: ${hueSliderValues[1].value}`;
-            secondaryColorSwatch.style.backgroundColor = `hsl(${hueSliderValues[1].value}, 100%, 50%)`;
-        };
-    }else if (slider.name === 'hue-tertiary') {
-        slider.oninput = () => {
-            hueTertiaryVal.innerHTML = `H: ${hueSliderValues[2].value}`;
-            tertiaryColorSwatch.style.backgroundColor = `hsl(${hueSliderValues[2].value}, 100%, 50%)`;
-        };
+// Assign atributes to input by selecting element and the attributes object
+function setInputAttributes(element, attributes) {
+
+    for(let key in attributes) {
+        element.setAttribute(key, attributes[key]);
     }
-});
 
-console.log(hueSliderValues);
+};
+
+
+// Create a color picker
+function createColorPicker() {
+
+    // Append all child elements to div container
+    colorTitleElement.appendChild(colorSwatch);
+    colorTitleElement.appendChild(hueSlider);
+    colorTitleElement.appendChild(satSlider);
+    colorTitleElement.appendChild(lightSlider);
+    sliderContainer.appendChild(colorTitleElement);
+    
+    // Assign attributes to hue, saturation, and light inputs
+    setInputAttributes(hueSlider, hueAttributes);
+    setInputAttributes(satSlider, satAttributes);
+    setInputAttributes(lightSlider, lightAttributes);
+
+    function updateSlider(slider) {
+
+        
+
+    }
+
+}
